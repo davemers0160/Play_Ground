b8fd7843930ffcb5219479fb83f054e3f9b9c897
David Emerson <davemers@umail.iu.edu>
2018-11-15 12:58:49 -0500

    testing ssim stuff
    - fond odd dlib behavior with matrix operations

--- src/pg_v2.cpp
+++ src/pg_v2.cpp
@@ -50,6 +50,8 @@
 #include "file_parser.h"
 #include "read_binary_image.h" 
 #include "make_dir.h"
+#include "ssim.h"
+#include "dlib_matrix_threshold.h"
 
 //#include "pso.h"
 //#include "ycrcb_pixel.h"
@@ -124,79 +126,57 @@ int main(int argc, char** argv)
 
 // ----------------------------------------------------------------------------------------
         
+
+        dlib::matrix<uint16_t> gt_img, tmp, tmp2, dm_img;
+        dlib::matrix<float> ssim_map;
+
         // load in the images --  not done in real life but for here
-        cv::Mat focus_image = cv::imread("D:/IUPUI/Test_Data/rw/Library2/left/exp_40/image_133_40.00.png", CV_LOAD_IMAGE_COLOR);
-        cv::Mat defocus_image = cv::imread("D:/IUPUI/Test_Data/rw/Library2/left/exp_40/image_130_40.00.png", CV_LOAD_IMAGE_COLOR);
+        std::string gt_image_name = "D:/IUPUI/Test_Data/rw/WS2/lidar/lidar_rng_right_00000_8bit.png";
+        std::string dm_image_name = "D:/IUPUI/PhD/Results/dfd_dnn_pso/itr1/dfd_pso_13/depthmap_image_v6_pso_13_01_test_00049.png";
 
-        // take the image and split the channels
-        // then copy each channel into one of the six inputs
-        // make sure that the right color gets placed in the right channel
-        std::vector<cv::Mat> f(3);
-        std::vector<cv::Mat> d(3);
-        std::array<dlib::matrix<uint16_t>, img_depth> input_img;
+        dlib::load_image(dm_img, dm_image_name);
+        dlib::load_image(tmp, gt_image_name);
 
-        //for (idx = 0; idx < 3; idx++)
-        //{
-        //    f[idx] = cv::Mat(imageSize, CV_8UC1);
-        //    YCRCB_OUT[idx] = cv::Mat(imageSize, CV_8UC1);
-        //}
+        tmp2.set_size(dm_img.nr(), dm_img.nc());
+        dlib::set_subm(tmp2, 0, 0, dm_img.nr(), dm_img.nc()) = dlib::subm(tmp, 0, 0, dm_img.nr(), dm_img.nc());
+                
+        truncate_threshold(tmp2, gt_img, 255);
 
-        //////// Split  images into 3 channels   /////////////////////////////////////////////////////////
+        auto s1 = dlib::sum(dlib::matrix_cast<float>(gt_img));
+        auto s2 = dlib::sum(dlib::matrix_cast<float>(dm_img));
 
-        start_time = chrono::system_clock::now();
-        cv::split(focus_image, f);
-        cv::split(defocus_image, d);
+        auto m_t = dlib::mean(dlib::matrix_cast<float>(gt_img));
+        uint16_t m = (uint16_t)(std::floor(m_t));
+        dlib::matrix<uint16_t> test_img = dlib::uniform_matrix<uint16_t>(dm_img.nr(), dm_img.nc(), m);
 
-        dlib::cv_image<uint8_t> tmp_img1(f[2]);
-        dlib::assign_image(input_img[0], tmp_img1);
-        
-        dlib::cv_image<uint8_t> tmp_img2(f[1]);
-        dlib::assign_image(input_img[1], tmp_img2);
+        dlib::matrix<uint16_t> t2 = dlib::uniform_matrix<uint16_t>(256, 256, 1);
 
-        dlib::cv_image<uint8_t> tmp_img3(f[0]);
-        dlib::assign_image(input_img[2], tmp_img3);
+        auto s1_t2 = dlib::sum(t2);
+        auto s2_t2 = dlib::sum(dlib::matrix_cast<float>(t2));
 
-        dlib::cv_image<uint8_t> tmp_img4(d[2]);
-        dlib::assign_image(input_img[3], tmp_img4);
+        auto m1_t2 = dlib::mean(t2);
+        auto m2_t2 = dlib::mean(dlib::matrix_cast<float>(t2));
 
-        dlib::cv_image<uint8_t> tmp_img5(d[1]);
-        dlib::assign_image(input_img[4], tmp_img5);
+        double ssim_dm = ssim(gt_img, dm_img, ssim_map);
+        double ssim_test = ssim(gt_img, test_img, ssim_map);
 
-        dlib::cv_image<uint8_t> tmp_img6(d[0]);
-        dlib::assign_image(input_img[5], tmp_img6);
-        
-        stop_time = chrono::system_clock::now();
+        dlib::matrix<float> sub_dm = dlib::matrix_cast<float>(gt_img) - dlib::matrix_cast<float>(dm_img);
+        dlib::matrix<float> sub_test = dlib::matrix_cast<float>(gt_img) - dlib::matrix_cast<float>(test_img);
 
-        elapsed_time = chrono::duration_cast<d_sec>(stop_time - start_time);
-        std::cout << "Test 1: " << elapsed_time.count() << " seconds" << std::endl;
+        double m1_dm = dlib::mean(dlib::abs(sub_dm));
+        double m2_dm = std::sqrt(dlib::mean(dlib::squared(sub_dm)));
 
-        bp = 1;
-        std::array<dlib::matrix<uint16_t>, img_depth> input_img2;
-        for (int m = 0; m < 6; ++m)
-        {
-            input_img2[m].set_size(focus_image.rows, focus_image.cols);
-        }
+        double m1_test = dlib::mean(dlib::abs(sub_test));
+        double m2_test = std::sqrt(dlib::mean(dlib::squared(sub_test)));
 
-        start_time = chrono::system_clock::now();
+        //double var = dlib::variance(gt[idx]);
+        double rng = 255;
+        double nmae_dm = m1_dm / rng;
+        double nrmse_dm = m2_dm / rng;
+        double nmae_te = m1_test / rng;
+        double nrmse_te = m2_test / rng;
 
-        for(idx = 0; idx<focus_image.rows; ++idx)
-        {
-            for (jdx = 0; jdx < focus_image.cols; ++jdx)
-            {
-                cv::Vec3w f1 = focus_image.at<cv::Vec3b>(idx, jdx);
-                dlib::assign_pixel(input_img2[0](idx, jdx), f1[2]);
-                dlib::assign_pixel(input_img2[1](idx, jdx), f1[1]);
-                dlib::assign_pixel(input_img2[2](idx, jdx), f1[0]);
-                cv::Vec3w d1 = focus_image.at<cv::Vec3b>(idx, jdx);
-                dlib::assign_pixel(input_img2[3](idx, jdx), d1[2]);
-                dlib::assign_pixel(input_img2[4](idx, jdx), d1[1]);
-                dlib::assign_pixel(input_img2[5](idx, jdx), d1[0]);
-            }
-        }
-
-        stop_time = chrono::system_clock::now();
-        elapsed_time = chrono::duration_cast<d_sec>(stop_time - start_time);
-        std::cout << "Test 2: " << elapsed_time.count() << " seconds" << std::endl;
+        //rmse_val += std::sqrt(m2) / rng;
 
         bp = 2;
 

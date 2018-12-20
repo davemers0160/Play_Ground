1110260486bfdf5911f60b78e375c3ad57621b0b
David Emerson <davemers@umail.iu.edu>
2018-12-20 10:32:03 -0500

    Update pg_v2.cpp

--- src/pg_v2.cpp
+++ src/pg_v2.cpp
@@ -63,8 +63,8 @@
 #include "dfd_cropper_rw.h"
 
 // Network includes
-//#include "dfd_net_rw_v6.h"
-//#include "load_dfd_rw_data.h"
+#include "dfd_net_rw_v6.h"
+#include "load_dfd_rw_data.h"
 
 using namespace std;
 
@@ -73,7 +73,7 @@ using namespace std;
 extern const uint32_t img_depth;
 extern const uint32_t secondary;
 std::string platform;
-//std::vector<std::array<dlib::matrix<uint16_t>, img_depth>> trn, te, trn_crop, te_crop;
+std::vector<std::array<dlib::matrix<uint16_t>, img_depth>> trn, te, trn_crop, te_crop;
 //std::vector<dlib::matrix<uint16_t>> gt_train, gt_test, gt_crop, gt_te_crop;
 
 std::string version;
@@ -83,6 +83,13 @@ std::string logfileName = "dfd_net_";
 std::string gorgon_savefile = "gorgon_dfd_";
 
 // ----------------------------------------------------------------------------------------
+
+template <typename img_type1>
+void check_matrix(img_type1 img)
+{
+    if (dlib::is_matrix<img_type1>::value == true)
+        std::cout << "matrix" << std::endl;
+}
 
 int main(int argc, char** argv)
 {
@@ -124,71 +131,18 @@ int main(int argc, char** argv)
     {
         int bp = 0;
 
-// ----------------------------------------------------------------------------------------
-        
-        std::vector<double> range;
-        parse_input_range("1:20:200", range);
 
+        std::array<dlib::matrix<uint16_t>, img_depth> img_t;
 
-        dlib::matrix<uint16_t> gt_img, tmp, tmp2, dm_img;
-        dlib::matrix<float> ssim_map;
+        for (idx = 0; idx < img_depth; ++idx)
+        {
+            img_t[idx] = dlib::zeros_matrix<uint16_t>(10, 10);
+        }
 
-        // load in the images --  not done in real life but for here
-        std::string gt_image_name = "D:/IUPUI/Test_Data/rw/Auditorium2/lidar/lidar_rng_left_00000_8bit.png";
-        std::string dm_image_name = "D:/IUPUI/PhD/Results/dfd_dnn_pso/itr1/dfd_pso_13/depthmap_image_v6_pso_13_01_test_00004.png";
+        dlib::matrix<double> img_a = dlib::zeros_matrix<double>(10, 10);
 
-        dlib::load_image(dm_img, dm_image_name);
-        dlib::load_image(tmp, gt_image_name);
-
-        tmp2.set_size(dm_img.nr(), dm_img.nc());
-        dlib::set_subm(tmp2, 0, 0, dm_img.nr(), dm_img.nc()) = dlib::subm(tmp, 0, 0, dm_img.nr(), dm_img.nc());
-                
-        truncate_threshold(tmp2, gt_img, 255);
-
-        auto s1 = dlib::sum(dlib::matrix_cast<float>(gt_img));
-        auto s2 = dlib::sum(dlib::matrix_cast<float>(dm_img));
-
-        auto m_t = dlib::mean(dlib::matrix_cast<float>(gt_img));
-        uint16_t m = (uint16_t)(std::floor(m_t));
-        dlib::matrix<uint16_t> test_img = dlib::uniform_matrix<uint16_t>(dm_img.nr(), dm_img.nc(), m);
-
-        dlib::matrix<uint16_t> t2 = dlib::uniform_matrix<uint16_t>(256, 256, 1);
-
-        auto s1_t2 = dlib::sum(t2);
-        auto s2_t2 = dlib::sum(dlib::matrix_cast<float>(t2));
-
-        auto m1_t2 = dlib::mean(t2);
-        auto m2_t2 = dlib::mean(dlib::matrix_cast<float>(t2));
-
-        double ssim_dm = ssim(gt_img, dm_img, ssim_map);
-        double ssim_test = ssim(gt_img, test_img, ssim_map);
-
-        dlib::matrix<float> sub_dm = dlib::matrix_cast<float>(gt_img) - dlib::matrix_cast<float>(dm_img);
-        dlib::matrix<float> sub_test = dlib::matrix_cast<float>(gt_img) - dlib::matrix_cast<float>(test_img);
-
-        double m1_dm = dlib::mean(dlib::abs(sub_dm));
-        double m2_dm = std::sqrt(dlib::mean(dlib::squared(sub_dm)));
-
-        double m1_test = dlib::mean(dlib::abs(sub_test));
-        double m2_test = std::sqrt(dlib::mean(dlib::squared(sub_test)));
-
-        //double var = dlib::variance(gt[idx]);
-        double rng = 255;
-        double nmae_dm = m1_dm / rng;
-        double nrmse_dm = m2_dm / rng;
-        double nmae_te = m1_test / rng;
-        double nrmse_te = m2_test / rng;
-
-        //rmse_val += std::sqrt(m2) / rng;
-
-        bp = 2;
-
-
-        float v1 = dlib::variance(dlib::matrix_cast<float>(gt_img));
-        float v2 = dlib::variance(dlib::matrix_cast<float>(dm_img));
-        float v3 = dlib::variance(dlib::matrix_cast<float>(test_img));
-
-        bp = 3;
+        check_matrix(img_a);
+        check_matrix(img_t);
 
 
     }
@@ -199,6 +153,7 @@ int main(int argc, char** argv)
         std::cin.ignore();
     }
 
+    std::cout << "Press Enter to close" << std::endl;
     std::cin.ignore();
 	return 0;
 

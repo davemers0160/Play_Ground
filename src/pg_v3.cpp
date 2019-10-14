c6713f474ff9d91b368d5f6619c94ac34e439c65
Dave <davemers@iu.edu>
2019-10-13 22:39:39 -0400

    playing with square detector

--- src/pg_v3.cpp
+++ src/pg_v3.cpp
@@ -38,10 +38,11 @@
 #include "dlib/gui_widgets.h"
 
 // OpenCV includes
-#include <opencv2/core/core.hpp>           
-#include <opencv2/highgui/highgui.hpp>     
-#include <opencv2/imgproc/imgproc.hpp> 
-#include <opencv2/video/video.hpp>
+#include <opencv2/core.hpp>           
+#include <opencv2/highgui.hpp>     
+#include <opencv2/imgproc.hpp> 
+#include <opencv2/video.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 // custom includes
 //#include "mmaplib.h"
@@ -107,6 +108,118 @@ std::string logfileName = "dfd_net_";
 std::string gorgon_savefile = "gorgon_dfd_";
 
 dlib::rand rnd(time(NULL));
+
+
+int thresh = 50, N = 11;
+const char* wndname = "Square Detection Demo";
+
+// helper function:
+// finds a cosine of angle between vectors
+// from pt0->pt1 and from pt0->pt2
+static double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
+{
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+}
+
+// returns sequence of squares detected on the image.
+static void findSquares(const cv::Mat& image, vector<vector<cv::Point> >& squares)
+{
+    squares.clear();
+
+    cv::Mat pyr, timg, gray0(image.size(), CV_8U), gray;
+
+    // down-scale and upscale the image to filter out the noise
+    cv::pyrDown(image, pyr, cv::Size(image.cols / 2, image.rows / 2));
+    cv::pyrUp(pyr, timg, image.size());
+    vector<vector<cv::Point> > contours;
+
+    // find squares in every color plane of the image
+    for (int c = 0; c < 3; c++)
+    {
+        int ch[] = { c, 0 };
+        cv::mixChannels(&timg, 1, &gray0, 1, ch, 1);
+
+        // try several threshold levels
+        for (int l = 0; l < N; l++)
+        {
+            // hack: use Canny instead of zero threshold level.
+            // Canny helps to catch squares with gradient shading
+            if (l == 0)
+            {
+                // apply Canny. Take the upper threshold from slider
+                // and set the lower to 0 (which forces edges merging)
+                cv::Canny(gray0, gray, 0, thresh, 5);
+                // dilate canny output to remove potential
+                // holes between edge segments
+                cv::dilate(gray, gray, cv::Mat(), cv::Point(-1, -1));
+            }
+            else
+            {
+                // apply threshold if l!=0:
+                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+                gray = gray0 >= (l + 1) * 255 / N;
+            }
+
+            // find contours and store them all as a list
+            cv::findContours(gray, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+
+            vector<cv::Point> approx;
+
+            // test each contour
+            for (size_t i = 0; i < contours.size(); i++)
+            {
+                // approximate contour with accuracy proportional
+                // to the contour perimeter
+                cv::approxPolyDP(contours[i], approx, cv::arcLength(contours[i], true) * 0.02, true);
+
+                // square contours should have 4 vertices after approximation
+                // relatively large area (to filter out noisy contours)
+                // and be convex.
+                // Note: absolute value of an area is used because
+                // area may be positive or negative - in accordance with the
+                // contour orientation
+                if (approx.size() == 4 &&
+                    fabs(contourArea(approx)) > 1000 &&
+                    isContourConvex(approx))
+                {
+                    double maxCosine = 0;
+
+                    for (int j = 2; j < 5; j++)
+                    {
+                        // find the maximum cosine of the angle between joint edges
+                        double cosine = fabs(angle(approx[j % 4], approx[j - 2], approx[j - 1]));
+                        maxCosine = MAX(maxCosine, cosine);
+                    }
+
+                    // if cosines of all angles are small
+                    // (all angles are ~90 degree) then write quandrange
+                    // vertices to resultant sequence
+                    if (maxCosine < 0.3)
+                        squares.push_back(approx);
+                }
+            }
+        }
+    }
+}
+
+
+// the function draws all the squares in the image
+static void drawSquares(cv::Mat& image, const vector<vector<cv::Point> >& squares)
+{
+    for (size_t i = 0; i < squares.size(); i++)
+    {
+        const cv::Point* p = &squares[i][0];
+        int n = (int)squares[i].size();
+        cv::polylines(image, &p, &n, 1, true, cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+    }
+
+    cv::imshow("test", image);
+}
+
 
 //using mnist_net_type = dlib::loss_multiclass_log<
 //    dlib::fc<10,
@@ -483,57 +596,25 @@ int main(int argc, char** argv)
             std::cout << "Path: " << exe_path << std::endl;
         #endif  
 
-        std::string text = "Funny text inside the box";
-        int fontFace = cv::FONT_HERSHEY_SIMPLEX;
-        double fontScale = 1.0;
-        int thickness = 2;
-        cv::Mat img(600, 140, CV_8UC3, cv::Scalar::all(0));
-        int baseline = 0;
 
-        dlib::matrix<dlib::rgb_pixel> img2 = dlib::uniform_matrix<dlib::rgb_pixel>(400, 400, dlib::rgb_pixel(0,0,0));
-
-        std::string label = "test_label";
-
-        dlib::rectangle rect(20, 20, 200, 200);
-        //overlay_bounding_box(dlib::toMat(img2), cv::Rect(20, 20, 200, 400), label, cv::Scalar(0, 0, 255));
-        dlib::rgb_pixel color(255, 0, 0);
-        dlib::mmod_rect box_label(rect, 0.0, label);
-
-        std::vector<dlib::mmod_rect> mmod_labels;
-
-        mmod_labels.push_back(box_label);
-
-        dlib::mmod_options options({ mmod_labels }, 60, 35, 0.75);
-
-        for (auto& w : options.detector_windows)
-            std::cout << "detector window width by height: " << w.width << " x " << w.height << ", " << w.label << std::endl;
-
-        overlay_bounding_box(img2, box_label, color);
-
-        dlib::image_window win0;
+        std::vector<std::vector<cv::Point> > squares;
 
 
-        win0.clear_overlay();
-        win0.set_image(img2);
-        //win0.add_overlay(rect, dlib::rgb_pixel(255, 0, 0));
-        //win0.add_overlay(dlib::rectangle(20,20,20,20), dlib::rgb_pixel(255, 0, 0), label);
+        std::string filename = "D:/Projects/mnist_viewer/input_test.png";
+
+        cv::Mat image = cv::imread(filename, cv::IMREAD_COLOR);
+
+        if (image.empty())
+        {
+            std::cout << "Couldn't load " << filename << std::endl;
+        }
+
+        findSquares(image, squares);
+        drawSquares(image, squares);
+
+        int c = cv::waitKey();
 
 
-        std::cin.ignore();
-
-/*
-        cv::Size text_size = cv::getTextSize(text, fontFace, fontScale, thickness, &baseline);
-        //baseline += thickness;
-
-        // center the text
-        cv::Point textOrg((img.cols - text_size.width) / 2, (img.rows + text_size.height) / 2);
-
-        // draw the box
-        cv::rectangle(img, textOrg + cv::Point(0, baseline), textOrg + cv::Point(text_size.width, -text_size.height), cv::Scalar(0, 0, 255), -1);
-        
-        // then put the text itself
-        cv::putText(img, text, textOrg, fontFace, fontScale, cv::Scalar(0,0,0), thickness, 8);
-*/
         bp = 3;
         //resnet_type net_101;
 

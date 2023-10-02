8ff6ba7b4699fc7129a56586e5343ec0d375b7fb
dre_beast <davemers0160@gmail.com>
2023-10-02 11:56:06 -0400

    rtsp stream test

--- mini_pg/src/mini_pg.cpp
+++ mini_pg/src/mini_pg.cpp
@@ -3,7 +3,7 @@
 
 //#include <yaml-cpp/yaml.h>
 //#define RYML_SINGLE_HDR_DEFINE_NOW
-#include <ryml_all.hpp>
+//#include <ryml_all.hpp>
 
 #if defined(_WIN32) | defined(__WIN32__) | defined(__WIN32) | defined(_WIN64) | defined(__WIN64)
 #include <windows.h>
@@ -43,6 +43,14 @@
 #include "modulo.h"
 //#include "console_input.h"
 
+#include "ocv_threshold_functions.h"
+
+// OpenCV includes
+#include <opencv2/core.hpp>           
+#include <opencv2/highgui.hpp>     
+#include <opencv2/imgproc.hpp> 
+#include <opencv2/video.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 #define M_PI 3.14159265358979323846
 #define M_2PI 6.283185307179586476925286766559
@@ -55,6 +63,38 @@ volatile bool entry = false;
 volatile bool run = true;
 std::string console_input1;
 
+const cv::Mat SE3_rect = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+const cv::Mat SE5_rect = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
+
+
+//----------------------------------------------------------------------------
+inline void get_rect(std::vector<cv::Point>& p, cv::Rect& r, int64_t img_w, int64_t img_h, int64_t x_padding = 40, int64_t y_padding = 40)
+{
+    uint64_t idx;
+    int64_t min_x = LLONG_MAX, min_y = LLONG_MAX;
+    int64_t max_x = 0, max_y = 0;
+
+    for (idx = 0; idx < p.size(); ++idx)
+    {
+        min_x = std::min(min_x, (int64_t)p[idx].x);
+        min_y = std::min(min_y, (int64_t)p[idx].y);
+        max_x = std::max(max_x, (int64_t)p[idx].x);
+        max_y = std::max(max_y, (int64_t)p[idx].y);
+
+    }
+
+    min_x = std::max(0LL, min_x - x_padding);
+    max_x = std::min(img_w, max_x + x_padding);
+
+    min_y = std::max(0LL, min_y - y_padding);
+    max_y = std::min(img_h, max_y + y_padding);
+
+    r = cv::Rect(min_x, min_y, max_x - min_x, max_y - min_y);
+
+}   // end of get_rect
+
+
+//----------------------------------------------------------------------------
 template<typename T>
 inline void vector_to_pair(std::vector<T> &v1, std::vector<T> &v2, std::vector<std::pair<T, T>> &p1)
 {
@@ -78,10 +118,10 @@ int main(int argc, char** argv)
 
     uint64_t idx=0, jdx=0;
 
-    typedef std::chrono::duration<double> d_sec;
-    auto start_time = chrono::system_clock::now();
-    auto stop_time = chrono::system_clock::now();
-    auto elapsed_time = chrono::duration_cast<d_sec>(stop_time - start_time);
+    typedef chrono::nanoseconds ns;
+    auto start_time = chrono::high_resolution_clock::now();
+    auto stop_time = chrono::high_resolution_clock::now();
+    auto elapsed_time = chrono::duration_cast<ns>(stop_time - start_time);
 
     int bp = 0;
 
@@ -105,40 +145,174 @@ int main(int argc, char** argv)
         uint32_t num_loops;
         uint32_t num_blocks;
         
+        uint32_t img_h = 320;
+        uint32_t img_w = 320;
+        double fps = 30;
+        int32_t four_cc = 0;
         
         num_loops = 100;
+
+        std::vector<std::vector<cv::Point> > img_contours;
+        std::vector<cv::Vec4i> img_hr;
+
+
         //num_blocks = std::ceil(num_loops / (double)num_threads);
 
-        start_time = chrono::system_clock::now();
-        num_threads = std::max(1U, std::thread::hardware_concurrency() - 1);
-        //{
-            std::vector<std::thread> threads(num_threads);
-            std::mutex critical;
-            for (int t = 0; t < num_threads; t++)
-            {
-                threads[t] = std::thread(std::bind([&](const int bi, const int ei, const int t)
-                {
+        /*
+        'appsrc ! videoconvert' + \
+    ' ! video/x-raw,format=I420' + \
+    ' ! x264enc speed-preset=ultrafast bitrate=600 key-int-max=' + str(fps * 2) + \
+    ' ! video/x-h264,profile=baseline' + \
+    ' ! rtspclientsink location=rtsp://localhost:8554/mystream'
+        */
+        
+        //std::string cap_string = "rtsp://192.168.1.150:8554/temp";
+        std::string cap_string = "rtsp://192.168.1.153:8554/camera-15";
+        cv::VideoCapture cap(cap_string);
+        cv::Mat input_frame;
+        cap >> input_frame;
 
-                    // loop over all items
-                    for (int idx = bi; idx < ei; ++idx)
-                    {
-                        //for (idx = 0; idx < num_loops; ++idx)
-                        {
-                            Sleep(500);
-                            std::lock_guard<std::mutex> lock(critical);
-                            std::cout << "Index: " << idx << std::endl;
-                        }
-                    }
-                }, t* num_loops / num_threads, (t + 1) == num_threads ? num_loops : (t + 1) * num_loops / num_threads, t));
+        img_h = input_frame.rows;
+        img_w = input_frame.cols;
+
+        std::string video_link;
+
+//        video_link = "appsrc ! videoconvert ! video/x-raw, format=I420, format=BGR ! x264enc speed-preset=ultrafast key-int-max=60 ! video/x-h264, profile=baseline ! rtspclientsink protocols=tcp location=rtsp://192.168.1.150:8554/mystream";
+        video_link = "appsrc ! videoconvert ! video/x-raw, format=I420 ! x264enc speed-preset=ultrafast key-int-max=60 ! video/x-h264, profile=baseline ! rtspclientsink protocols=tcp location=rtsp://192.168.1.153:8554/mystream";
+        //video_link = "appsrc ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency ! video/x-h264,profile=high ! flvmux ! udpsink host=192.168.1.150/mystream port=8554";
+        //video_link = "appsrc ! videoconvert ! udpsink host=192.168.1.150:8554/mystream ";
+
+        cv::VideoWriter writer(video_link, cv::CAP_GSTREAMER, four_cc, fps, cv::Size(img_w, img_h), true);
+
+
+        start_time = chrono::high_resolution_clock::now();
+        num_threads = std::max(1U, std::thread::hardware_concurrency() - 1);
+
+        cv::Mat previous_frame = cv::Mat::zeros(img_h, img_w, CV_32FC1);
+
+        cv::Mat output_frame;
+        cv::Mat temp_frame;
+        cv::Mat mask_img;
+        cv::Mat mask_invert = cv::Mat(img_h, img_w, CV_32FC3, cv::Scalar(1, 1, 1));
+
+        //cv::cvtColor(output_frame, output_frame, cv::COLOR_BGR2RGB);
+
+        cv::Mat block(100, 100, CV_8UC3, cv::Scalar(0, 0, 0));
+        cv::Rect block_rect(400, 150, block.cols, block.rows);
+
+        cv::RNG rng;
+
+        cv::namedWindow("test", cv::WINDOW_NORMAL);
+        cv::namedWindow("input", cv::WINDOW_NORMAL);
+        cv::namedWindow("diff_frame", cv::WINDOW_NORMAL);
+
+        //cap >> input_frame;
+        //cv::imwrite("baseline_img.png", input_frame);
+
+        cv::Mat baseline_img = cv::imread("baseline_img.png");
+        baseline_img.convertTo(baseline_img, CV_32FC1);
+
+        cv::Rect overlay_rect;
+
+        char key = 0;
+
+        while (key != 'q')
+        {
+            start_time = chrono::high_resolution_clock::now();
+            cap >> input_frame;
+
+            if (input_frame.empty())
+                continue;
+
+            cv::imshow("input", input_frame);
+
+            mask_img = cv::Mat::zeros(img_h, img_w, CV_32FC3);
+
+            //rng.fill(block, cv::RNG::UNIFORM, 0, 255);
+            //cv::rectangle(input_frame, cv::Rect(20, 130, 150, 120), cv::Scalar(0, 0, 0), -1);
+            //block.copyTo(input_frame(block_rect));
+
+            output_frame = input_frame.clone();
+            cv::cvtColor(input_frame, input_frame, cv::COLOR_BGR2GRAY);
+            input_frame.convertTo(input_frame, CV_32FC1);
+            // blur the image using a sigma == 1.0 with border reflection
+            cv::GaussianBlur(input_frame, input_frame, cv::Size(0, 0), 1.5, 1.5, cv::BORDER_REFLECT_101);
+            cv::absdiff(input_frame, previous_frame, temp_frame);
+            advanced_threshold(temp_frame, 6, 0.0f, 255.0f);
+
+            cv::morphologyEx(temp_frame, temp_frame, cv::MORPH_DILATE, SE5_rect);
+            cv::morphologyEx(temp_frame, temp_frame, cv::MORPH_CLOSE, SE5_rect);
+
+            temp_frame.convertTo(temp_frame, CV_8UC1);
+            cv::imshow("diff_frame", temp_frame);
+
+            // find the contours of the remaining shapes
+            cv::findContours(temp_frame, img_contours, img_hr, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+            for (idx = 0; idx < img_contours.size(); ++idx)
+            {
+                get_rect(img_contours[idx], overlay_rect, img_w, img_h);
+
+                cv::rectangle(mask_img, overlay_rect, cv::Scalar(1,1,1), -1);
+
+                //baseline_img(overlay_rect).copyTo(output_frame(overlay_rect));
 
             }
-            std::for_each(threads.begin(), threads.end(), [](std::thread& x) {x.join(); });
+
+            cv::GaussianBlur(mask_img, mask_img, cv::Size(0, 0), 2.0, 2.0, cv::BORDER_REFLECT_101);
+            output_frame.convertTo(output_frame, CV_32FC3);
+            output_frame = baseline_img.mul(mask_img) + output_frame.mul(mask_invert - mask_img);
+            output_frame.convertTo(output_frame, CV_8UC3);
+
+            //output_frame.convertTo(output_frame, CV_8UC3);
+            //cv::cvtColor(output_frame, output_frame, cv::COLOR_GRAY2BGR);
+
+            writer << output_frame;
+
+            cv::imshow("test", output_frame);
+            key = cv::waitKey(1);
+
+            previous_frame = input_frame.clone();
+
+            do
+            {
+                stop_time = chrono::high_resolution_clock::now();
+                elapsed_time = chrono::duration_cast<ns>(stop_time - start_time);
+            } while (elapsed_time.count() < 1 / fps);
+
+        }
+
+
+
+
+        //{
+            //std::vector<std::thread> threads(num_threads);
+            //std::mutex critical;
+            //for (int t = 0; t < num_threads; t++)
+            //{
+            //    threads[t] = std::thread(std::bind([&](const int bi, const int ei, const int t)
+            //    {
+
+            //        // loop over all items
+            //        for (int idx = bi; idx < ei; ++idx)
+            //        {
+            //            //for (idx = 0; idx < num_loops; ++idx)
+            //            {
+            //                Sleep(500);
+            //                std::lock_guard<std::mutex> lock(critical);
+            //                std::cout << "Index: " << idx << std::endl;
+            //            }
+            //        }
+            //    }, t* num_loops / num_threads, (t + 1) == num_threads ? num_loops : (t + 1) * num_loops / num_threads, t));
+
+            //}
+            //std::for_each(threads.begin(), threads.end(), [](std::thread& x) {x.join(); });
 
         //}
-        stop_time = chrono::system_clock::now();
+        stop_time = chrono::high_resolution_clock::now();
 
 
-        elapsed_time = chrono::duration_cast<d_sec>(stop_time - start_time);
+        elapsed_time = chrono::duration_cast<ns>(stop_time - start_time);
 
         std::cout << "elapsed_time " << elapsed_time.count() << std::endl;
 
@@ -151,6 +325,9 @@ int main(int argc, char** argv)
     {
         std::cout << e.what() << std::endl;
     }
+
+
+    cv::destroyAllWindows();
 
     std::cout << std::endl << "Press Enter to close" << std::endl;
     std::cin.ignore();
